d5b38144da53e81b2bdd3ea0bd3e282a665f74e1
Nellynma <nellyxtabel@gmail.com>
2023-01-09 00:08:46 -0800

    Finds all multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,27 +1,9 @@
-#include <stdio.h>
-#include <Python.h>
-
-/**
- * print_python_list_info - prints python list info
- *
- * @p: PyObject
- * Return: no return
- */
-void print_python_list_info(PyObject *p)
-{
-    long int size, i;
-    PyListObject *list;
-    PyObject *item;
-
-    size = Py_SIZE(p);
-    printf("[*] Size of the Python List = %ld\n", size);
-
-    list = (PyListObject *)p;
-    printf("[*] Allocated = %ld\n", list->allocated);
-
-    for (i = 0; i < size; i++)
-    {
-        item = PyList_GetItem(p, i);
-        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
-}
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        if ((my_list[i] % 2) == 0):
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return (new_list)
